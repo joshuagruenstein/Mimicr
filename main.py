179982944fb812320faac7b5a5cc4ebfbbe7a1ea
joshuagruenstein <joshuagruenstein@gmail.com>
2015-11-23 19:18:39 -0500

    backend organization

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from werkzeug.wrappers import Request, Response
 from threading import Thread
 from time import sleep
+import numpy as np
+
+outputs = []
+
+def newThread(instring):
+	num = 5
+	return str(5)
 
 @Request.application
 def application(request):
 	instring = request.args.get('input', 'noinput')
-	print instring
+	index = request.args.get('index','noindex')
 
-	return Response("hi")
+	if index == 'noindex':
+		newdex = newThread(instring)
+		return Response(newdex)
+
+	mostRecent = outputs[int(index)]
+	return Response(mostRecent)
 
 if __name__ == '__main__':
 	from werkzeug.serving import run_simple
