a4a8d8d72ff81e3af55c46c2c9a801d27bb6dc95
joshuagruenstein <joshuagruenstein@gmail.com>
2015-11-24 20:26:44 -0500

    errorbox and endthread

--- script/index.js
+++ script/index.js
@@ -2,21 +2,30 @@ var outdiv = document.getElementById("outdiv")
 var indiv = document.getElementById("indiv")
 
 function createThread(input) {
-	return 5;
+	// @truell20 you can add this stuff
+
+	return 5
 }
 
 function endThread(index) {
-
+	// @truell20 this as well
 }
 
 document.getElementById('begin').onclick = function() {
+	var errorBox = document.getElementById("errorbox")
 	var sample = document.getElementById("sample").value
-	var index = createThread(sample)
-	Cookies.set('mimicr',index,{ expires: 2 })
+	if (sample.length >= 100000) {
+		errorBox.style.display = "none"
+		var index = createThread(sample)
 
-	outdiv.style.display = "block"
-	indiv.style.display = "none"
-};
+		if (document.getElementById('cookiecheck').checked) {
+			Cookies.set('mimicr',index,{ expires: 2 })
+		} else gIndex = index;
+
+		outdiv.style.display = "block"
+		indiv.style.display = "none"
+	} else errorBox.style.display = "block"
+}
 
 document.getElementById('stop').onclick = function() {
 	endThread(Cookies.get('mimicr'))
@@ -24,6 +33,14 @@ document.getElementById('stop').onclick = function() {
 
 	outdiv.style.display = "none"
 	indiv.style.display = "block"
+}
+
+window.onbeforeunload = function(e) {
+	if (outdiv.style.display === "block") {
+		if (! Cookies.get('mimicr')) {
+			endThread(gIndex)
+		}
+	} 
 }
 
 if (! Cookies.get('mimicr')) {
